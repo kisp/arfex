634c94e71735a2b34f14c5846d460c09890669af
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-22 13:44:45 +0200

    ex11 pdf

--- ex11/ex11.lisp
+++ ex11/ex11.lisp
@@ -1,1 +1,25 @@
 (+ 2 3)
+
+(with-output-to-file (output "g.dot" :if-exists :supersede)
+  (write-line "digraph g
+        {
+
+rankdir=LR;
+
+A[style=\"filled\",fillcolor=\"white\"];
+B[style=\"filled\",fillcolor=\"white\"];
+C[style=\"filled\",fillcolor=\"white\"];
+D[style=\"filled\",fillcolor=\"white\"];
+E[style=\"filled\",fillcolor=\"white\"];
+A -> A;
+A -> B;
+B -> D;
+D -> C;
+C -> B;
+E -> D;
+D -> E;
+}
+"
+              output))
+
+(uiop:run-program "dot -Tpdf g.dot >g.pdf")
