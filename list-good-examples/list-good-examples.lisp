9f960920a8db62c918f930fce9a52b9900068bd8
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-24 11:37:31 +0200

    Create list-good-examples.lisp

---
+++ list-good-examples/list-good-examples.lisp
@@ -0,0 +1,22 @@
+(load-data)
+
+(length (list-good-examples (generate-non-isomorphic 2 :irreflexive t)))
+(length (list-good-examples (generate-non-isomorphic 2 :irreflexive nil)))
+
+
+(length (list-good-examples (generate-non-isomorphic 3 :irreflexive t)))
+(length (list-good-examples (generate-non-isomorphic 3 :irreflexive nil)))
+
+
+(length (list-good-examples (generate-non-isomorphic 4 :irreflexive t)))
+(length (list-good-examples (generate-non-isomorphic 4 :irreflexive nil)))
+
+(length (list-good-examples *4-ir*))
+(length (list-good-examples *4*))
+
+
+(length (list-good-examples *5-ir*))
+(length (list-good-examples *5*))
+
+
+(length (list-good-examples *6-ir*))
