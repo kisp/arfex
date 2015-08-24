c9c4be07bcd724a98ef2ff8a98d450b69aac33fe
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-24 14:39:00 +0200

    Create load-data-hash.lisp

---
+++ load-data-hash/load-data-hash.lisp
@@ -0,0 +1,25 @@
+(ql:quickload "md5")
+
+(deftype octet () '(unsigned-byte 8))
+
+(load-data)
+
+(list-data)
+
+(defun calc-hash! (list)
+  (let ((state (md5:make-md5-state)))
+    (dolist (item (sort list #'< :key #'cdr))
+      (let* ((string
+               (with-output-to-string (output)
+                 (print item output)))
+             (vector (make-array (length string)
+                                 :element-type 'octet
+                                 :initial-contents (map 'list #'char-code string))))
+        (md5:update-md5-state state vector)))
+    (md5:finalize-md5-state state)))
+
+(dolist (symbol (list-data))
+  (print symbol)
+  (print (length (symbol-value symbol)))
+  (print (calc-hash! (symbol-value symbol)))
+  (terpri))
