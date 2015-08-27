6e6871ab4dfeb92ffadbbc740c9633bafd1d471a
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-27 19:35:14 +0200

    Update test_pl2af01.lisp

--- test_pl2af01/test_pl2af01.lisp
+++ test_pl2af01/test_pl2af01.lisp
@@ -1,6 +1,10 @@
-(pl2af '(x (not y) (imp x y)))
+(defparameter *g* (pl2af '(x (not y) (imp x y))))
+
+(defparameter *args* (nth-value 1 (pl2af '(x (not y) (imp x y)))))
 
 (with-open-file (out "af.dot" :direction :output :if-exists :supersede)
-  (print-af-to-dot (pl2af '(x (not y) (imp x y))) out))
+  (print-af-to-dot *g* out))
 
 (uiop:run-program "dot -Tpdf af.dot >af.pdf")
+
+(list-extensions *g* #'stable-extension-p)
